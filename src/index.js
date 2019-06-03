4a477bb39374ac1bf173c387119afcadb9cefdf9
Junior Dugue <jrdugue@gmail.com>
2019-06-03 13:59:53 -0400

    added handleTap function
    added function to detect when the toggle is clicked

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Frame } from "framer";
+import { Frame } from "framer"; //from the framer libarary
 
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <Frame width={120} height={60} borderRadius={60}>
+      <Frame
+        width={120}
+        height={60}
+        borderRadius={60}
+        onTap={function handleTap() {
+          //change the value to x = {0}
+          console.log("tapped!");
+        }}
+      >
+        {/* have to camel case border-radius, for height/width you can use % or rem*/}
         <Frame size={60} borderRadius={30} x={60} />
+        {/*x is like x translate in css */}
       </Frame>
     </div>
   );
